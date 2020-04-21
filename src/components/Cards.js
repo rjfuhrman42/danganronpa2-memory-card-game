c73282cee0b2f305d01daf9017ac3aa806340df0
Reid Fuhrman <rjfuhrman42@students.tntech.edu>
2020-04-21 18:23:17 -0500

    added working timer, end screen, etc.

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,18 +3,16 @@ import Card from "./Card"
 import images from "../images"
 
 function Cards () {
-
+    const STARTING_TIME = 90
     const [cards, setCards] = useState([])
     const [flippedCards, setFlippedCards] = useState([])
     const [matchedCards, setMatchedCards] = useState([])
-    const [gameOver, setGameOver] = useState(false)
+    const [turns, setTurns] = useState(0)
+    const [playing, setPlaying] = useState(false)
+    const [time, setTime] = useState(STARTING_TIME)
 
     useEffect(() => {
-        let array = [...images, ...images]                           // double the images array, so that we have two of each card)
-
-        array = shuffle(array)
-
-        setCards(array)                                             // shuffle the cards on page load 
+        setupGame()
     }, [])
 
     useEffect(() => {
@@ -23,6 +21,8 @@ function Cards () {
                 let firstImage = cards[flippedCards[0]] 
                 let secondImage = cards[flippedCards[1]]
 
+                setTurns(prev => prev + 1)
+
                 if(firstImage === secondImage) {
                     setMatchedCards([...matchedCards, flippedCards[0], flippedCards[1]])
                     setFlippedCards([])
@@ -30,18 +30,33 @@ function Cards () {
                 else {
                     setFlippedCards([])
                 }
-            }, 1000)
+            }, 500)
         }
     }, [flippedCards])
 
     useEffect(() => {
 
         if(matchedCards.length === images.length * 2) 
-        setGameOver(true)
+        setPlaying(false)
 
     }, [matchedCards])
 
+    useEffect(() => {
+        if(playing && time > 0) 
+        {
+            setTimeout(() => setTime(prev => prev - 1), 1000)
+        }
+        else if (time === 0)
+        {
+            setPlaying(false)
+        }
+    }, [time, playing])
+
     function handleClick(id) {
+
+        if(!playing) {
+            setPlaying(true)
+        }
         // if less than two cards flipped, this card is not currently flipped, and this card has not already been successfully matched
         if(flippedCards.length < 2 && !flippedCards.includes(id) && !matchedCards.includes(id))  
         {
@@ -65,18 +80,28 @@ function Cards () {
         return temp
     }
 
+    function setupGame() {
+        let array = [...images, ...images]                           // double the images array, so that we have two of each card)
+
+        array = shuffle(array)
+
+        setCards(array) 
+        setMatchedCards([])
+        setFlippedCards([])          
+        setTurns(0)
+        setTime(STARTING_TIME)
+    }
+
     function winGame() {
         let matches = []
         cards.forEach((card, index) => matches.push(index))
         setMatchedCards(matches)
     }
 
-    if(gameOver)
+    if(time > 0 && matchedCards.length < images.length * 2)
     return (
-        <h1>YOU WIN!</h1>
-    )
-    else return (
         <div className={'cards-grid'}>
+            {console.log(matchedCards.length, images.length*2)}
            { cards.map((image, index) => (
                 <Card front={image} 
                       flipped={flippedCards.includes(index)}
@@ -86,7 +111,18 @@ function Cards () {
                 />
                 )
             )}
+            <div className="game-menu">
+                <p>{playing ? `TIME: ${time} TURNS:${turns}` : "CLICK A CARD TO START..."}</p>
+            </div>
             <button onClick={() => winGame()}>WIN!</button>
+        </div>
+    )
+    else return (
+        <div className="game-over-display">
+            <h1>{matchedCards.length === images.length * 2 ? 'YOU WIN!' : 'TIMES UP!'}</h1>
+            <h1>TURNS: {turns}</h1>
+            <h1>TIME: {STARTING_TIME - time}</h1>
+            <button onClick={setupGame}>PLAY AGAIN?</button>
         </div>
     )
 }
