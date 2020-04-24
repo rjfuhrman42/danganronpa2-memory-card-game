848c394b9c4f4abfbdc6c535552233d8d4d57503
Reid Fuhrman <rjfuhrman42@students.tntech.edu>
2020-04-24 15:37:49 -0500

    site is now live, also works on mobile

---
+++ public/precache-manifest.6b9622c7b9312ef2874702e5d7d5d550.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5050f3603881b5a19202c3566c77720",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a57dd6e365c64696228",
+    "url": "/static/css/main.858fd8ce.chunk.css"
+  },
+  {
+    "revision": "e52d1598f6aa840b97d2",
+    "url": "/static/js/2.6da4d34f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6da4d34f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a57dd6e365c64696228",
+    "url": "/static/js/main.2fb4c7c7.chunk.js"
+  },
+  {
+    "revision": "7606da4c80d22da094a6",
+    "url": "/static/js/runtime-main.f664d84b.js"
+  },
+  {
+    "revision": "1c0a5ceafe590964b058939814e06256",
+    "url": "/static/media/akane.1c0a5cea.png"
+  },
+  {
+    "revision": "0a13b1f48495021bdba064d4da66f7fe",
+    "url": "/static/media/byakuya.0a13b1f4.png"
+  },
+  {
+    "revision": "e1a01aba2341cab37e4c5c8de05c0074",
+    "url": "/static/media/chiaki.e1a01aba.png"
+  },
+  {
+    "revision": "2f0d3eb1b99f33473a2b4077823d3b37",
+    "url": "/static/media/crt_green_mask.2f0d3eb1.png"
+  },
+  {
+    "revision": "ba769d22f5333ab276040b6d96fcc956",
+    "url": "/static/media/fuyuhiko.ba769d22.png"
+  },
+  {
+    "revision": "c2241e3fd64204f1dcee637004273ee0",
+    "url": "/static/media/gundham.c2241e3f.png"
+  },
+  {
+    "revision": "cbdfead8222b624e9cdd0e7966743838",
+    "url": "/static/media/hajime.cbdfead8.png"
+  },
+  {
+    "revision": "1e0405c838a0f53ed9449125adf9bf39",
+    "url": "/static/media/hiyoko.1e0405c8.png"
+  },
+  {
+    "revision": "92643a501629d624b9564a0f10f9d9c7",
+    "url": "/static/media/ibuki.92643a50.png"
+  },
+  {
+    "revision": "c626e844798c1b6e1914a8ecaa9cb06c",
+    "url": "/static/media/mahiru.c626e844.png"
+  },
+  {
+    "revision": "32506a4f635d1c018e8b9b6df5a9c5ae",
+    "url": "/static/media/mikan.32506a4f.png"
+  },
+  {
+    "revision": "39df86a3ebd6112b5a69983de1e8da54",
+    "url": "/static/media/nagito.39df86a3.png"
+  },
+  {
+    "revision": "43a912fb02b154d2dfcd2c380cec75df",
+    "url": "/static/media/nekomaru.43a912fb.png"
+  },
+  {
+    "revision": "eb4bbb2081ed85bc067c1d112bace7b8",
+    "url": "/static/media/peko.eb4bbb20.png"
+  },
+  {
+    "revision": "347a231dcbb586a1dc8e38ae54ee8b39",
+    "url": "/static/media/soda.347a231d.png"
+  },
+  {
+    "revision": "f40cae0b213609f70ad1840d2d46dc65",
+    "url": "/static/media/sonya.f40cae0b.png"
+  },
+  {
+    "revision": "4c751a4d6028c059be0b49055a425d8b",
+    "url": "/static/media/teruteru.4c751a4d.png"
+  }
+]);
